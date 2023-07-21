1cacdfcd4b2bebb15909fd06843bcf44d83ae76a
Michael Haji <michaelh1277@gmail.com>
2023-07-20 17:31:46 -0700

    update index.html, script.js and styles.css

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ function setupBillTracker () {
   const billTable = document.getElementById('bill-list')
   const billNameInput = document.getElementById('bill-name')
   const billAmountInput = document.getElementById('bill-amount')
+  const billDateInput = document.getElementById('bill-date') // Add billDateInput
   const billTotalDiv = document.getElementById('bill-total')
+  const filterInput = document.getElementById('filter-input')
+  const searchInput = document.getElementById('search-input')
+
+  // Helper function to format the date as "month, day, year"
+  function formatDate (dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' }
+    return new Date(dateString).toLocaleDateString(undefined, options)
+  }
 
   // Event listener for form submission
   billForm.addEventListener('submit', function (event) {
@@ -14,6 +23,7 @@ function setupBillTracker () {
     // Get input values
     const billName = billNameInput.value
     const billAmount = billAmountInput.value
+    const billDate = formatDate(billDateInput.value) // Correct variable name and format date
 
     // Create new row in the table using template literals
     const newRow = document.createElement('tr')
@@ -21,6 +31,7 @@ function setupBillTracker () {
     newRow.innerHTML = `
       <td>${billName}</td>
       <td>$${billAmount}</td>
+      <td>${billDate}</td>
       <td>
         <button class="delete-btn">Delete</button>
       </td>
@@ -32,6 +43,7 @@ function setupBillTracker () {
     // Clear input fields
     billNameInput.value = ''
     billAmountInput.value = ''
+    billDateInput.value = ''
 
     // Update total after adding a bill
     updateTotal()
@@ -57,6 +69,57 @@ function setupBillTracker () {
     const totalAmount = billAmounts.reduce((acc, curr) => acc + curr, 0)
     billTotalDiv.innerText = `Total: $${totalAmount.toFixed(2)}`
   }
+
+  // Event listener for filter input changes
+  filterInput.addEventListener('input', function () {
+    const filterValue = filterInput.value.toLowerCase()
+
+    // Loop through each bill row and check if it matches the filter value
+    const billRows = document.querySelectorAll('.bill-item')
+    billRows.forEach(row => {
+      const billName = row
+        .querySelector('td:nth-child(1)')
+        .innerText.toLowerCase()
+      const billAmount = row
+        .querySelector('td:nth-child(2)')
+        .innerText.toLowerCase()
+      const billDate = row
+        .querySelector('td:nth-child(3)')
+        .innerText.toLowerCase()
+
+      if (
+        billName.includes(filterValue) ||
+        billAmount.includes(filterValue) ||
+        billDate.includes(filterValue)
+      ) {
+        row.style.display = 'table-row'
+      } else {
+        row.style.display = 'none'
+      }
+    })
+  })
+
+  // Event listener for search input changes
+  searchInput.addEventListener('input', function () {
+    const searchValue = searchInput.value.toLowerCase()
+
+    // Loop through each bill row and check if it matches the search value
+    const billRows = document.querySelectorAll('.bill-item')
+    billRows.forEach(row => {
+      const billName = row
+        .querySelector('td:nth-child(1)')
+        .innerText.toLowerCase()
+
+      if (billName.includes(searchValue)) {
+        row.style.display = 'table-row'
+      } else {
+        row.style.display = 'none'
+      }
+    })
+  })
+
+  // Call the function to set up the bill tracker once the document has loaded
+  updateTotal()
 }
 
 // Call the function to set up the bill tracker once the document has loaded
